e30c7382afbcd1cc370e34b2e4f55b4b61da1fe9
smeb y <48400087+a3165458@users.noreply.github.com>
2024-05-21 15:23:18 +0800

    Update Artela.sh

--- Artela.sh
+++ Artela.sh
@@ -190,16 +190,17 @@ function add_validator() {
     read -p "请输入您的钱包名称: " wallet_name
     read -p "请输入您想设置的验证者的名字: " validator_name
     
-artelad tx staking create-validator --node $Artela_RPC_PORT \
---amount 1000000uart \
---from $wallet_name \
---min-self-delegation 1 \
---pubkey $(artelad tendermint show-validator) \
---moniker "$validator_name" \
---identity "" \
---details "" \
---chain-id artela_11822-1 \
---gas 300000 \
+artelad tx staking create-validator \
+--amount="1art" \
+--pubkey=$(artelad tendermint show-validator) \
+--moniker="$validator_name" \
+--commission-rate="0.10" \
+--commission-max-rate="0.20" \
+--commission-max-change-rate="0.01" \
+--min-self-delegation="1" \
+--gas="200000" \
+--chain-id="artela_11822-1" \
+--from="$wallet_name" \
 
 }
 

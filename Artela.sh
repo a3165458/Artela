9534f91ff293889946322b00831671ebb87c1fae
smeb y <48400087+a3165458@users.noreply.github.com>
2024-05-26 22:34:50 +0800

    Update Artela.sh

--- Artela.sh
+++ Artela.sh
@@ -209,7 +209,7 @@ artelad tx staking create-validator \
 function delegate_self_validator() {
 read -p "请输入质押代币数量: " math
 read -p "请输入钱包名称: " wallet_name
-artelad tx staking delegate $(artelad keys show $wallet_name --bech val -a)  ${math}art --from $wallet_name --chain-id=artela_11822-1 --gas=300000 --node $Artela_RPC_PORT  -y
+artelad tx staking delegate $(artelad keys show $wallet_name --bech val -a)  ${math}art --from $wallet_name --chain-id=artela_11822-1 --gas=300000
 
 }
 

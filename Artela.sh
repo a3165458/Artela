a61e8fa410700445b92ed91855b0c54e5b4fe537
smeb y <48400087+a3165458@users.noreply.github.com>
2024-06-14 10:30:35 +0800

    Update Artela.sh

--- Artela.sh
+++ Artela.sh
@@ -119,7 +119,7 @@ function install_node() {
     
     # 下载快照
     artelad tendermint unsafe-reset-all --home $HOME/.artelad --keep-addr-book
-    curl -L https://snapshots.dadunode.com/artela/artela_latest_tar.lz4 | tar -I lz4 -xf - -C $HOME/.artelad
+    curl -L https://snapshots.dadunode.com/artela/artela_latest_tar.lz4 | tar -I lz4 -xf - -C $HOME/.artelad/data
     mv $HOME/.artelad/priv_validator_state.json.backup $HOME/.artelad/data/priv_validator_state.json
 
     # 使用 PM2 启动节点进程

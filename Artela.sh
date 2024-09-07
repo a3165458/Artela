929f6a99f88ba6b2556649a9230a6ccb25410222
smeb y <48400087+a3165458@users.noreply.github.com>
2024-09-07 16:32:41 +0800

    Update Artela.sh

--- Artela.sh
+++ Artela.sh
@@ -256,7 +256,6 @@ wget https://github.com/artela-network/artela/releases/download/v0.4.9-rc9/artel
 tar -xvf artelad_v0.4.9-rc9_Linux_amd64.tar.gz
 cp artelad /usr/local/bin/artelad && cp artelad /root/go/bin/artelad
 mv $HOME/libaspect_wasm_instrument.so $HOME/libs/
-mv $HOME/artelad /usr/local/bin/
 echo 'export LD_LIBRARY_PATH=$HOME/libs:$LD_LIBRARY_PATH' >> ~/.bash_profile
 source ~/.bash_profile
 wget -O ~/.artelad/data/priv_validator_state.json  https://snapshots.dadunode.com/artela/priv_validator_state.json

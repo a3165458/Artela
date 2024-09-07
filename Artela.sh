27379c4277df824e405ff7dc317cf3bb961955c8
smeb y <48400087+a3165458@users.noreply.github.com>
2024-09-07 18:24:47 +0800

    Update Artela.sh

--- Artela.sh
+++ Artela.sh
@@ -248,14 +248,20 @@ function update_node() {
 
 pm2 delete artelad
 
-mv ~/.artelad ~/artelad_back_up
-rsync -av --exclude "data" ~/artelad_back_up/* ~/.artelad
+cd && rm -rf artela
+git clone https://github.com/artela-network/artela
+cd artela
+LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+git checkout $LATEST_TAG
+make install
 
 cd $HOME
 wget https://github.com/artela-network/artela/releases/download/v0.4.9-rc9/artelad_v0.4.9-rc9_Linux_amd64.tar.gz
 tar -xvf artelad_v0.4.9-rc9_Linux_amd64.tar.gz
 cp artelad /usr/local/bin/artelad && cp artelad /root/go/bin/artelad
+mkdir libs
 mv $HOME/libaspect_wasm_instrument.so $HOME/libs/
+mv $HOME/artelad /usr/local/bin/
 echo 'export LD_LIBRARY_PATH=$HOME/libs:$LD_LIBRARY_PATH' >> ~/.bash_profile
 source ~/.bash_profile
 wget -O ~/.artelad/data/priv_validator_state.json  https://snapshots.dadunode.com/artela/priv_validator_state.json
